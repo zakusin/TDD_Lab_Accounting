8d69bc41ca3c6f3c89d1f14d7cbaf472592f3f6a
zakusin <zakusin@gmail.com>
2019-12-21 15:43:37 +0800

    add model

---
+++ Accounting/Accounting/Budget.cs
@@ -0,0 +1,8 @@
+﻿namespace Accounting
+{
+    public class Budget
+    {
+        public string YearMonth { get; set; }
+        public int Amount { get; set; }
+    }
+}
---
+++ Accounting/Accounting/IBudgetRepo.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Accounting
+{
+    public interface IBudgetRepo
+    {
+        List<Budget> GetAll();
+    }
+}
