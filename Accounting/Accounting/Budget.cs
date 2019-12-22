6e173e79d43c26b5ac04ad57e4fd3fd2c6009eb7
zakusin <zakusin@gmail.com>
2019-12-22 11:56:17 +0800

    rename budget start & end day

--- Accounting/Accounting/Budget.cs
+++ Accounting/Accounting/Budget.cs
@@ -6,17 +6,17 @@ namespace Accounting
     {
         public string YearMonth { get; set; }
         public int Amount { get; set; }
-        private DateTime BudgetFirstDate => DateTime.ParseExact(YearMonth + "01", "yyyyMMdd", null);
-        private DateTime BudgetEndDate => BudgetFirstDate.AddMonths(1).AddDays(-1);
-        private int BudgetDays => DateTime.DaysInMonth(BudgetFirstDate.Year, BudgetFirstDate.Month);
+        private DateTime BudgetFirstDay => DateTime.ParseExact(YearMonth + "01", "yyyyMMdd", null);
+        private DateTime BudgetLastDay => BudgetFirstDay.AddMonths(1).AddDays(-1);
+        private int BudgetDays => DateTime.DaysInMonth(BudgetFirstDay.Year, BudgetFirstDay.Month);
 
         public decimal EffectiveBudget(DateTime start, DateTime end)
         {
-            var effectiveStartDate = start < BudgetFirstDate 
-                ? BudgetFirstDate 
+            var effectiveStartDate = start < BudgetFirstDay 
+                ? BudgetFirstDay 
                 : start;
-            var effectiveEndDate = end > BudgetEndDate 
-                ? BudgetEndDate 
+            var effectiveEndDate = end > BudgetLastDay 
+                ? BudgetLastDay 
                 : end;
             var effectiveDays = (effectiveEndDate - effectiveStartDate).Days + 1;
             return Amount * effectiveDays / BudgetDays;
